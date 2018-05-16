7430093548aa70d3e7b80b94d3c450facbefd3a4
Hitesh V <hiteshpratyush@gmail.com>
2018-05-16 15:49:28 +0530

    MQTT Setup

--- app/src/main/java/com/sendrecv/ble/blesendandrecieve/PublishActivity.java
+++ app/src/main/java/com/sendrecv/ble/blesendandrecieve/PublishActivity.java
@@ -2,12 +2,124 @@ package com.sendrecv.ble.blesendandrecieve;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class PublishActivity extends AppCompatActivity {
 
+    Button pubConnect,pubDisconnect,pubPub;
+    EditText messagemqtt;
+    String clientId;
+    MqttAndroidClient client;
+    MqttConnectOptions options;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish);
+        pubConnect=(Button)findViewById(R.id.pubConnect);
+        pubPub=(Button)findViewById(R.id.pubpub);
+        pubDisconnect=(Button)findViewById(R.id.pubDisconnect);
+        messagemqtt=(EditText)findViewById(R.id.messagemqtt) ;
+        clientId = MqttClient.generateClientId();
+
+
+        pubConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client = new MqttAndroidClient(getApplicationContext(), "tcp://m11.cloudmqtt.com:16515", clientId);
+                options = new MqttConnectOptions();
+                options.setUserName("vsptbtrg");
+                options.setPassword("Zb5IXem-B2Rw".toCharArray());
+                try {
+                    IMqttToken token = client.connect(options);
+                    token.setActionCallback(new IMqttActionListener() {
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+                            Log.d("Connection", "Connected to Broker ");
+                            //Toast.makeText(getApplicationContext(),"ConnectiontoMQTTBrokerMade", Toast.LENGTH_SHORT).show();
+                            pubConnect.setVisibility(View.INVISIBLE);
+                            pubDisconnect.setVisibility(View.VISIBLE);
+                            pubPub.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                            //Toast.makeText(getApplicationContext(),"ConnectiontoMQTTBrokerRejected", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(),exception.toString(),Toast.LENGTH_LONG).show();
+                            Log.d("Connection", "Unable to connect to Broker");
+                        }
+                    });
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        pubDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                disconnect();
+            }
+        });
+
+        pubPub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String payload = messagemqtt.getText().toString();
+                byte[] encodedPayload;
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(getString(R.string.topic), message);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(pubConnect.getVisibility()==View.INVISIBLE)
+            disconnect();
+    }
+
+    public void disconnect()
+    {
+        try {
+            IMqttToken disconToken = client.disconnect();
+            disconToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    pubPub.setVisibility(View.INVISIBLE);
+                    pubDisconnect.setVisibility(View.INVISIBLE);
+                    pubConnect.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    // something went wrong, but probably we are disconnected anyway
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/sendrecv/ble/blesendandrecieve/SubscribeActivity.java
+++ app/src/main/java/com/sendrecv/ble/blesendandrecieve/SubscribeActivity.java
@@ -3,59 +3,195 @@ package com.sendrecv.ble.blesendandrecieve;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.ByteBuffer;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SubscribeActivity extends AppCompatActivity {
+    Button subConnect,subDisconnect,subSub,subUnsub;
+    ListView messageListView;
+    ArrayList<String> messageList;
+    ArrayAdapter adapter;
+    String clientId;
+    MqttAndroidClient client;
+    MqttConnectOptions options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscribe);
-        String clientId = MqttClient.generateClientId();
-        MqttAndroidClient client =
-                new MqttAndroidClient(this.getApplicationContext(), "m11.cloudmqtt.com:16515",
-                        clientId);
-        MqttConnectOptions options = new MqttConnectOptions();
-        options.setUserName("vsptbtrg");
-        options.setPassword("Zb5IXem-B2Rw".toCharArray());
+        subConnect=(Button)findViewById(R.id.subConnect);
+        subSub=(Button)findViewById(R.id.subsub);
+        subUnsub=(Button)findViewById(R.id.subunsub);
+        subDisconnect=(Button)findViewById(R.id.subDisconnect);
+        messageListView=(ListView)findViewById(R.id.messageListView);
+        messageList=new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this,R.layout.message_list,messageList);
+        clientId = MqttClient.generateClientId();
+        messageListView.setAdapter(adapter);
 
+
+        subConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client = new MqttAndroidClient(getApplicationContext(), "tcp://m11.cloudmqtt.com:16515", clientId);
+                options = new MqttConnectOptions();
+                options.setUserName("vsptbtrg");
+                options.setPassword("Zb5IXem-B2Rw".toCharArray());
+                try {
+                    IMqttToken token = client.connect(options);
+                    token.setActionCallback(new IMqttActionListener() {
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+                            Log.d("Connection", "Connected to Broker ");
+                            //Toast.makeText(getApplicationContext(),"ConnectiontoMQTTBrokerMade", Toast.LENGTH_SHORT).show();
+                            subConnect.setVisibility(View.INVISIBLE);
+                            subDisconnect.setVisibility(View.VISIBLE);
+                            subSub.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                            //Toast.makeText(getApplicationContext(),"ConnectiontoMQTTBrokerRejected", Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(getApplicationContext(),exception.getMessage(),Toast.LENGTH_LONG).show();
+                            Log.d("Connection", "Unable to connect to Broker");
+                        }
+                    });
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                client.setCallback(new MqttCallback() {
+                    @Override
+                    public void connectionLost(Throwable throwable) {
+                        //do nothing
+                    }
+
+                    @Override
+                    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+                        adapter.add(new String(mqttMessage.getPayload()));
+                    }
+
+                    @Override
+                    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                        //do nothing
+                    }
+                });
+            }
+        });
+
+        subDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(subSub.getVisibility()==View.INVISIBLE) {
+                    subUnsub.setVisibility(View.INVISIBLE);
+                    subSub.setVisibility(View.VISIBLE);
+                    unsubscribe();
+                }
+                disconnect();
+            }
+        });
+
+        subSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int qos = 1;
+                try {
+                    IMqttToken subToken = client.subscribe(getString(R.string.topic), qos);
+                    subToken.setActionCallback(new IMqttActionListener() {
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+                            subSub.setVisibility(View.INVISIBLE);
+                            subUnsub.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken,
+                                              Throwable exception) {
+                            // The subscription could not be performed, maybe the user was not
+                            // authorized to subscribe on the specified topic e.g. using wildcards
+
+                        }
+                    });
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        subUnsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unsubscribe();
+                subUnsub.setVisibility(View.INVISIBLE);
+                subSub.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(subSub.getVisibility()==View.INVISIBLE && subConnect.getVisibility()==View.INVISIBLE)
+            unsubscribe();
+        if(subConnect.getVisibility()==View.INVISIBLE)
+            disconnect();
+    }
+
+    public void disconnect()
+    {
         try {
-            IMqttToken token = client.connect(options);
-            token.setActionCallback(new IMqttActionListener() {
+            IMqttToken disconToken = client.disconnect();
+            disconToken.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    Log.d("ConnectiontoMQTTBroker", "onSuccess");
+                    subSub.setVisibility(View.INVISIBLE);
+                    subDisconnect.setVisibility(View.INVISIBLE);
+                    subConnect.setVisibility(View.VISIBLE);
                 }
 
                 @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    Log.d("ConnectiontoMQTTBroker", "onFailure");
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    // something went wrong, but probably we are disconnected anyway
                 }
             });
         } catch (MqttException e) {
             e.printStackTrace();
         }
+    }
 
-        String topic = "foo/bar";
-        String payload = "the payload";
-        byte[] encodedPayload = new byte[0];
+    public void unsubscribe()
+    {
         try {
-            encodedPayload = payload.getBytes("UTF-8");
-            MqttMessage message = new MqttMessage(encodedPayload);
-            message.setRetained(true);
-            client.publish(topic, message);
-        } catch (UnsupportedEncodingException | MqttException e) {
+            IMqttToken unsubToken = client.unsubscribe(getString(R.string.topic));
+            unsubToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                }
+            });
+        } catch (MqttException e) {
             e.printStackTrace();
         }
     }
