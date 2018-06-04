eaa0f8aafe7a88adec1ee77c2728a34ad3915802
Hitesh V <hiteshpratyush@gmail.com>
2018-06-04 16:53:46 +0530

    Websocket Working.

--- app/src/main/java/com/sendrecv/ble/blesendandrecieve/WebSocketActivity.java
+++ app/src/main/java/com/sendrecv/ble/blesendandrecieve/WebSocketActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.codebutler.android_websockets.WebSocketClient;
@@ -14,6 +16,7 @@ import org.apache.http.message.BasicNameValuePair;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -21,9 +24,13 @@ public class WebSocketActivity extends AppCompatActivity {
 
     public final String TAG = "WebSocketClient";
     Button connect,disconnect,send;
-    TextView messages;
-    EditText getIP;
+    EditText getIP,getMessage;
     WebSocketClient client;
+    ListView messageList;
+    ArrayList<String> messages;
+    ArrayAdapter adapter;
+    List<BasicNameValuePair> extraHeaders = null;
+    boolean flag=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,63 +40,109 @@ public class WebSocketActivity extends AppCompatActivity {
         connect=(Button)findViewById(R.id.wsconnect);
         send = (Button)findViewById(R.id.wssend);
         disconnect = (Button)findViewById(R.id.wsdisconnect);
-        messages=(TextView)findViewById(R.id.wsmessages);
         getIP = (EditText)findViewById(R.id.getIP);
-        List<BasicNameValuePair> extraHeaders = null;
-
-        client = new WebSocketClient(URI.create("ws://"+getIP.getText().toString()), new WebSocketClient.Listener() {
-            @Override
-            public void onConnect() {
-                Log.d(TAG, "Connected!");
-            }
-
-            @Override
-            public void onMessage(String message) {
-                Log.d(TAG, String.format("Got string message! %s", message));
-            }
-
-            @Override
-            public void onMessage(byte[] data) throws UnsupportedEncodingException {
-                Log.d(TAG, String.format("Got binary message! %s", new String(data,"UTF-8")));
-            }
-
-            @Override
-            public void onDisconnect(int code, String reason) {
-                Log.d(TAG, String.format("Disconnected! Code: %d Reason: %s", code, reason));
-            }
-
-            @Override
-            public void onError(Exception error) {
-                Log.e(TAG, "Error -"+error.toString() );
-            }
-        }, extraHeaders);
+        getMessage = (EditText)findViewById(R.id.getMessage);
+        messageList = (ListView)findViewById(R.id.messageList);
+        messages=new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this,R.layout.message_list,messages);
+        messageList.setAdapter(adapter);
 
 
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                client = new WebSocketClient(URI.create("ws://"+getIP.getText().toString()), new WebSocketClient.Listener() {
+
+                    @Override
+                    public void onConnect() {
+                        Log.d(TAG, "Connected!");
+                        connect();
+                    }
+
+                    @Override
+                    public void onMessage(String message) {
+                        send(message);
+                        Log.d(TAG, String.format("Got string message! %s", message));
+                    }
+
+                    @Override
+                    public void onMessage(byte[] data) throws UnsupportedEncodingException {
+                        send(new String(data,"UTF-8"));
+                        Log.d(TAG, String.format("Got binary message! %s", new String(data,"UTF-8")));
+                    }
+
+                    @Override
+                    public void onDisconnect(int code, String reason) {
+                        Log.d(TAG, String.format("Disconnected! Code: %d Reason: %s", code, reason));
+                        disconnect();
+                    }
+
+                    @Override
+                    public void onError(Exception error) {
+                        Log.e(TAG, "Error -"+error.toString() );
+                        disconnect();
+                    }
+                }, extraHeaders);
                 client.connect();
-                send.setVisibility(View.VISIBLE);
-                disconnect.setVisibility(View.VISIBLE);
-                connect.setVisibility(View.INVISIBLE);
             }
         });
+
+
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                client.send("hello!");
+                client.send(getMessage.getText().toString());
             }
         });
+
+
         disconnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 client.disconnect();
-                disconnect.setVisibility(View.INVISIBLE);
-                send.setVisibility(View.INVISIBLE);
-                connect.setVisibility(View.VISIBLE);
             }
         });
 
+    }
+
+    private void connect()
+    {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                adapter.clear();
+                send.setVisibility(View.VISIBLE);
+                disconnect.setVisibility(View.VISIBLE);
+                getMessage.setVisibility(View.VISIBLE);
+                connect.setVisibility(View.INVISIBLE);
+                getIP.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    private void disconnect()
+    {
+        client=null;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                disconnect.setVisibility(View.INVISIBLE);
+                send.setVisibility(View.INVISIBLE);
+                getMessage.setVisibility(View.INVISIBLE);
+                connect.setVisibility(View.VISIBLE);
+                getIP.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void send(final String message)
+    {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                adapter.add(message);
+            }
+        });
     }
 
 }
